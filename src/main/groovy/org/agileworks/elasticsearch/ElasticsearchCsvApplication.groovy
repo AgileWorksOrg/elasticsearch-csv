dacbb645443b6e383843310734d7b9c357f93110
Vitek Tajzich <v.tajzich@gmai.com>
2016-01-17 12:01:56 +0100

    initial commit

---
+++ src/main/groovy/org/agileworks/elasticsearch/ElasticsearchCsvApplication.groovy
@@ -0,0 +1,12 @@
+package org.agileworks.elasticsearch
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ElasticsearchCsvApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run ElasticsearchCsvApplication, args
+    }
+}
